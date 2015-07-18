c50348ac13159da48a528a0289851b4bc09abc6e
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-18 12:51:02 +0530

    Added model project

---
+++ SimpleDataMVC/SimpleDataMVC.Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleDataMVC.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
